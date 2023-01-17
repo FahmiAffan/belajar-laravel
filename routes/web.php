209d485c47e516830ad7663f6d6dcc4322b12a11
Muhammad Fahmi Affan <fahmiaffan07@gmail.com>
2023-01-17 19:02:52 +0700

    First Project

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\SiswaController;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/data-siswa' , [SiswaController::class, 'index']);
+
+Route::post('/tambah-siswa' , [SiswaController::class, 'create']);
