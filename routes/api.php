dffd13a40afbcad81d8d81c53c8588364c4fa726
Fahmi Affan <fahmiaffan07@gmail.com>
2023-02-25 01:52:15 +0700

    JWT Auth Login + Logout

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\BukuController;
 use App\Http\Controllers\GuruController;
@@ -45,12 +46,9 @@ Route::controller(TestingController::class)->group(function () {
     Route::put('/test/{id}', 'updatedata');
 });
 
-//JWT-Authx
-
-/**
- * route "/register"
- * @method "POST"
- */
+//JWT-Auth
 Route::post('/register',RegisterController::class)->name('register');
 
 Route::post('/login', LoginController::class)->name('login');
+
+Route::post('/logout', LogoutController::class)->name('logout');
